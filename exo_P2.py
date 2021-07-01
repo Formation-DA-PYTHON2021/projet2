2d78065ab59b00cde4e3b74e27b9980e00ba90cd
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2021-07-01 10:40:35 +0000

    debug consigne

--- exo_P2.py
+++ exo_P2.py
@@ -5,10 +5,11 @@ from bs4 import BeautifulSoup
 # Récupérer les url de chaque page
 links = []
 
-for i in range(51):
+for i in range(1,52):
     print(i)
 
     url = 'https://books.toscrape.com/catalogue/page-' + str(i) +'.html'
+#     print(url)
     reponse = requests.get(url)
 
     if reponse.ok :
@@ -18,13 +19,12 @@ for i in range(51):
             a = article.find('a')
             link = a['href']
             links.append('https://books.toscrape.com/catalogue/' + link)
-            
-with open('urls.txt','w') as file
+
+with open('urls.txt','w') as file:
         for link in links :
                 file.write(link + '\n')
 
-"""
-"""
+
 # Récupérer les informations demandées dans les pages
 url = 'https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
 reponse = requests.get(url)
@@ -33,36 +33,40 @@ if reponse.ok:
         soup = BeautifulSoup(reponse.text, 'html.parser')
         UPC = soup.find_all('td')[0].text
         titre = soup.find('h1').text
-        #price_in = soup.find_all('td')[3].text.replace('Â'; '') #j'aimerai suprimé le 'Â' qui est devant
-        #price_ex = soup.find_all('td')[2].text.replace('Â'; '') #j'aimerai suprimé le 'Â' qui est devant
+        #price_in = soup.find_all('td')[3].text.replace('Â','') #j'aimerai suprimé le 'Â' qui est devant
+        #price_ex = soup.find_all('td')[2].text.replace('Â','') #j'aimerai suprimé le 'Â' qui est devant
         available = soup.find_all('td')[5].text
         descrip = soup.find_all('p')[3].text
         cat = soup.find_all('a')[3].text
         rating = soup.find_all('p', class_= 'star-rating Three') #j'aimerai récupérer juste le texte "three"
-        image = soup.find('img') #je n'arrive pas à extraire juste l'url ; je ne sais pas si juste url ou image ?
-
-""
-with open('urls.text', 'r') as inf
-                with open('livrable2.csv', 'w') as outf :
-                        outf.write(print('product_page_url, universal_product_code, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url\n')
-                        for row in outf
-                                url = row.strip()
-                                reponse = requests.get(url)
-                                if reponse.ok:
-                                        soup = BeautifulSoup(reponse.text, 'html.parser')
-                                        UPC = soup.find_all('td')[0].text
-                                        titre = soup.find('h1').text
-                                        # price_in = soup.find_all('td')[3].text.replace('Â'; '') #j'aimerai suprimé le 'Â' qui est devant
-                                        # price_ex = soup.find_all('td')[2].text.replace('Â'; '') #j'aimerai suprimé le 'Â' qui est devant
-                                        available = soup.find_all('td')[5].text
-                                        descrip = soup.find_all('p')[3].text
-                                        cat = soup.find_all('a')[3].text
-                                        rating = soup.find_all('p', class_='star-rating Three')  # j'aimerai récupérer juste le texte "three"
-                                        image = soup.find('img')  # je n'arrive pas à extraire juste l'url ; je ne sais pas si juste url ou image ?
-                                        file.write(links.text + ',' + UPC + ',' + titre + ',' + price_in + ',' + price_ex + ',' + available + ','+ descrip ','+ cat + ',' + rating + ',' + image + '\n')
-                        print('product_page_url'+ links.text + 'universal_product_code: ' + UPC + 'title : ' + titre + 'price_including_tax : ' + price_in + 'price_excluding_tax :' + price_ex + 'number_available:' + available + 'product_description:'+ descrip 'category:'+ cat + 'review_rating:' + rating + 'image_url :' + image)
-
-""
+        image = soup.find('img')['src'].replace('../../', 'https://books.toscrape.com/') #je n'arrive pas à extraire juste l'url ; je ne sais pas si juste url ou image ?
 
 
-
+with open('urls.txt', 'r') as inf:
+    with open('livrable2.csv', 'w') as outf :
+        outf.write(print('product_page_url, universal_product_code, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url\n'))
+        for row in outf:
+                url = row.strip()
+                reponse = requests.get(outf)
+                if reponse.ok:
+                        soup = BeautifulSoup(reponse.text, 'html.parser')
+                        UPC = soup.find_all('td')[0].text
+                        titre = soup.find('h1').text
+                        price_in = soup.find_all('td')[3].text.replace('Â','') #j'aimerai suprimé le 'Â' qui est devant
+                        price_ex = soup.find_all('td')[2].text.replace('Â','') #j'aimerai suprimé le 'Â' qui est devant
+                        print(price_ex, price_in)
+                        available = soup.find_all('td')[5].text
+                        descrip = soup.find_all('p')[3].text
+                        cat = soup.find_all('a')[3].text
+                        image_div2 = soup.find('div', attrs={'class': 'col-sm-6 product_main'})
+                        #Recuperation du rating
+                        rating = image_div2.select('p.star-rating')
+                        for i in rating:
+                                rating = i.attrs['class'][-1]
+                        print(rating)
+                        # rating = soup.find_all('p', class_='star-rating Three')  # j'aimerai récupérer juste le texte "three"
+                        image = soup.find('img')['src'].replace('../../', 'https://books.toscrape.com/') # je n'arrive pas à extraire juste l'url ; je ne sais pas si juste url ou image ?
+                        print(image)
+                        file.write(links.text + ',' + UPC + ',' + titre + ',' + price_in + ',' + price_ex + ',' + available + ','+ descrip +','+ cat + ',' + rating + ',' + image + '\n')
+                print('product_page_url'+ links.text + 'universal_product_code: ' + UPC + 'title : ' + titre + 'price_including_tax : ' + price_in + 'price_excluding_tax :' + price_ex + 'number_available:' + available + 'product_description:'+ descrip +'category:'+ cat + 'review_rating:' + rating + 'image_url :' + image)
+"""
