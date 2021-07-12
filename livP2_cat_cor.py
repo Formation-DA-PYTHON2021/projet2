57b408ac0a391dc1bbdac51362d5a596a8b0eac3
Tiffany CALVET <tiffany.calvet@developpement-durable.gouv.fr>
2021-07-12 11:42:03 +0200

    apport correction pour prise en compte du nombre de page par catégories

---
+++ livP2_cat_cor.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+links = []
+page_url = []
+UPC = []
+titre = []
+price_in = []
+price_ex = []
+available = []
+descrip = []
+cat = []
+rating = []
+image = []
+
+for i in range(1,8) :
+    url = 'https://books.toscrape.com/catalogue/category/books/mystery_3/page-' + str(i) + '.html'
+    reponse = requests.get(url)
+    if reponse.ok:
+        soup = BeautifulSoup(reponse.text, 'html.parser')
+        articles = soup.findAll('article')
+        for article in articles:
+            a = article.find('a')
+            link = a['href'].replace('../../../', '')
+            links.append('https://books.toscrape.com/catalogue/' + link)
